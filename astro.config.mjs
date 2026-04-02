03291a294115b19b9e7844f860adf88c2cb94236
xBirahim <65773874+xBirahim@users.noreply.github.com>
2026-04-02 17:11:13 +0200

    chore(favicon): replace favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: "MS Teams Observability",
+            favicon: "/favicon.ico",
 			lastUpdated: true,
 			defaultLocale: "root",
 			locales: {
